059e433e4e71d44774cfad43068a7b1bac3a4c50
Monika H J <monikahj7@gmail.com>
2025-07-04 16:18:38 +0530

    perosnal detais done

--- src/app/_components/candidate/PersonalDetails.tsx
+++ src/app/_components/candidate/PersonalDetails.tsx
@@ -21,39 +21,42 @@ const PersonalDetails = ({ onNext }: Props) => {
       gender: "",
       education: ""
     }
-    if(!name.trim()){
-      newErrors.name= "Email is required";
+    if (!name.trim()) {
+      newErrors.name = "Email is required";
 
     }
-    if(!phone.trim()){
-      newErrors.phone= "Contact Number is require";
+    if (!phone.trim()) {
+      newErrors.phone = "Contact Number is require";
     }
-    if(!email.trim()){
-      newErrors.email="email is required";
+    if (!email.trim()) {
+      newErrors.email = "email is required";
     }
-    if(!gender.trim()){
-      newErrors.gender= "gender is required";
-    }if(!education.trim()){ 
+    if (!gender.trim()) {
+      newErrors.gender = "gender is required";
+    } if (!education.trim()) {
       newErrors.education = "Education is required"
     }
     setErrors(newErrors);
-    return Object.values(newErrors).every((val)=>val==="");
+    return Object.values(newErrors).every((val) => val === "");
   }
 
   const { name, phone, email, gender, education } = state.personal;
   return (
     <div>
-      <div>
-        <label className="mb-2 block text-xl font-semibold">Name</label>
-        <input type="text"
-          value={name}
-          onChange={(e) =>
-            dispatch({ type: "SET_PERSONAL", payload: { name: e.target.value } })
-          }
-          className="w-full rounded border border-gray-500 p-2 focus:ring-2 focus:ring-fuchsia-300 focus:outline-none"
-          placeholder="Enter Your Name"
-        />
-        {errors.name && <p className="text-sm text-red-500"></p>}
+
+      <div className="">
+        <div>
+          <label className="mb-2 block text-xl font-semibold">Name</label>
+          <input type="text"
+            value={name}
+            onChange={(e) =>
+              dispatch({ type: "SET_PERSONAL", payload: { name: e.target.value } })
+            }
+            className="w-full rounded border border-gray-500 p-2 focus:ring-2 focus:ring-fuchsia-300 focus:outline-none"
+            placeholder="Enter Your Name"
+          />
+          {errors.name && <p className="text-sm text-red-500">{errors.name}</p>}
+        </div>
       </div>
 
       <div>
@@ -66,7 +69,7 @@ const PersonalDetails = ({ onNext }: Props) => {
           className="w-full rounded border border-gray-500 p-2 focus:ring-2 focus:ring-fuchsia-300 focus:outline-none"
           placeholder="Enter Your Phone"
         />
-        {errors.phone && <p className="text-sm text-red-500"></p>}
+        {errors.phone && <p className="text-sm text-red-500">{errors.phone}</p>}
       </div>
       <div>
         <label className="mb-2 block text-xl font-semibold">Email</label>
@@ -78,7 +81,7 @@ const PersonalDetails = ({ onNext }: Props) => {
           className="w-full rounded border border-gray-500 p-2 focus:ring-2 focus:ring-fuchsia-300 focus:outline-none"
           placeholder="Enter Your Email"
         />
-        {errors.email && <p className="text-sm text-red-500">{errors.name}</p>}
+        {errors.email && <p className="text-sm text-red-500">{errors.email}</p>}
       </div>
 
 
@@ -99,6 +102,7 @@ const PersonalDetails = ({ onNext }: Props) => {
         {errors.gender && <p className="text-sm text-red-500">{errors.gender}</p>}
       </div>
 
+
       <div>
         <label className="mb-2 block text-xl font-semibold">Education</label>
         <select
@@ -118,6 +122,19 @@ const PersonalDetails = ({ onNext }: Props) => {
         {errors.education && (
           <p className="text-sm text-red-500">{errors.education}</p>
         )}
+      </div>
+
+      <div className="flex justify-center mt-20 gap-10">
+        <div className="">
+          <button
+            onClick={() => validateForm()}
+            className="border bg-fuchsia-500 rounded-2xl p-2 text-3xl ">Save</button>
+        </div>
+        <div className="">
+          <button
+            onClick={() => validateForm()}
+            className="border bg-fuchsia-500 rounded-2xl p-2 text-3xl">Next</button>
+        </div>
       </div>
 
     </div>
