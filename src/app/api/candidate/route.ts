72a34cfadfa7b99d34da02468d9e338e16ce9e50
Monika H J <monikahj7@gmail.com>
2025-07-09 16:18:09 +0530

    making prisma

---
+++ src/app/api/candidate/route.ts
@@ -0,0 +1,6 @@
+
+// i need this to send sucees/error from API route browser 
+import { NextResponse } from "next/server";
+
+// Import prisma client to talk to the database 
+import {db} from "~/server/db"
