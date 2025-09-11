3cfec3958a4a679c86fe480cea7346ec9c5f7018
Monika H J <monikahj7@gmail.com>
2025-09-11 13:25:27 +0530

    Previw Job UI

---
+++ src/app/_components/post_job/PostJob_preview.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PostJob_preview = () => {
+  return (
+    <div className='mt-10 px-4 sm:px-10 md:px-24'>
+      <div className='rounded-2xl bg-white p-6 shadow-lg sm:p-10 md:p-16'>
+         <h2 className="mb-8 text-center text-2xl font-bold text-fuchsia-700">
+              Preview Page
+            </h2>
+
+      </div>
+    </div>
+  )
+}
+
+export default PostJob_preview
