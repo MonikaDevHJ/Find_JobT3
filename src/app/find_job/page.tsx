d3b18dc4cd4f7bd8d57430d49b89d9ca90e47123
Monika H J <monikahj7@gmail.com>
2025-10-08 13:16:19 +0530

    filerr section

--- src/app/find_job/page.tsx
+++ src/app/find_job/page.tsx
@@ -1,22 +1,40 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 import Filter from "../_components/find_job/Filter"
 import JobList from "../_components/find_job/JobList"
+
+type FiltersState = {
+  [key: string] : string[];
+}
+
+
 const findJob = () => {
+  
+  // here we will store the whatever the filter the user selects
+  const [selectedFilters, setSelectedFilters] = useState<FiltersState>({});
+
   return (
     <div className=' text-center '>
-      {/* Search Bar */}
 
       {/* Flex container for Filter and JobList */}
       <div className="flex flex-col md:flex-row gap-4 mt-4 max-w-6xl mx-auto p-1">
         {/* Left: Filter */}
         <div className="w-full md:w-1/4">
-          <Filter />
+        {/* pass this state to filter */}
+          <Filter 
+          selectedFilters={selectedFilters} 
+          setSelectedFilters={setSelectedFilters}
+           />
         </div>
 
         {/* Right: Job List */}
         <div className="w-full md:w-3/4">
-          <JobList />
+        {/* Also pass this state to JObList */}
+          <JobList  
+          selectedFilters={selectedFilters} 
+          />
         </div>
       </div>
     </div>
