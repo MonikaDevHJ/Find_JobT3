cba49020f979f120f3aced1a5758d83fbf90dade
Monika H J <monikahj7@gmail.com>
2025-11-18 13:27:47 +0530

    done

--- src/app/api/employerlistofjobs/route.ts
+++ src/app/api/employerlistofjobs/route.ts
@@ -1,28 +1,35 @@
 import { NextResponse } from "next/server";
 import { db } from "~/server/db";
 import { auth } from "@clerk/nextjs/server";
+import { join } from "path";
 
-
-export async function  GET() {
-    try{
-        const {userId} = await auth();
-        if(!userId){
-            return NextResponse.json({message : "Unauthorized"}, {status:401})
-        }
-        const myJobs = await db.jobPost.findMany({
-            where:{
-                clerkId:userId,
-            },
-            orderBy : {createdAt: "desc"}
-        });
-        return NextResponse.json(myJobs)
-
-    }catch (err : any){
-        return NextResponse.json(
-            {message: "Error fetching employer jobs", error: err.message},
-            {status:500}
-        )
-
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
-    
+    const myJobs = await db.jobPost.findMany({
+      where: {
+        clerkId: userId,
+      },
+      include: {
+        appliedcandidate: true,
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    // Add appliedCount
+    const formatted = myJobs.map((job) => ({
+      ...job,
+      appliedCount: job.appliedcandidate.length,
+    }));
+
+    return NextResponse.json(formatted);
+  } catch (err: any) {
+    return NextResponse.json(
+      { message: "Error fetching employer jobs", error: err.message },
+      { status: 500 },
+    );
+  }
 }
