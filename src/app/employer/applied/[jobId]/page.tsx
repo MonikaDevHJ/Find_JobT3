0cc8ac46e030accc2733d9fc48061b6e46dfd329
Monika H J <monikahj7@gmail.com>
2025-12-04 17:00:48 +0530

    done

--- src/app/employer/applied/[jobId]/page.tsx
+++ src/app/employer/applied/[jobId]/page.tsx
@@ -1,5 +1,7 @@
-import AppliedCandidates from "~/app/_components/employer/applied/[jobId]/AppliedCandidates";
+import AppliedCandidates from "./AppliedCandidates";
 
-export default function Page({ params }: { params: { jobId: string } }) {
-  return <AppliedCandidates jobId={params.jobId} />;
+export default async function Page({ params }: { params: Promise<{ jobId: string }> }) {
+  const { jobId } = await params;
+
+  return <AppliedCandidates jobId={jobId} />;
 }
