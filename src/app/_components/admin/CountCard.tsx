88333cd7f5071343d656d622ca8b845ee2363a93
Monika H J <monikahj7@gmail.com>
2025-12-15 16:06:37 +0530

    done

---
+++ src/app/_components/admin/CountCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Eye } from "lucide-react";
+
+type CountcardProps = {
+  title: string;
+  count: number;
+  loading?: boolean;
+};
+
+const CountCard: React.FC<CountcardProps> = ({
+  title,
+  count,
+  loading = false,
+}) => {
+  return (
+    <div className="rounded-2xl border border-gray-200 bg-white p-8 shadow-lg transition hover:-translate-y-1 hover:shadow-xl">
+      <div className="flex items-center justify-between">
+        <p className="text-xl font-semibold text-gray-800">{title}</p>
+        <Eye
+          className="cursor-pointer text-fuchsia-700 transition hover:text-fuchsia-500"
+          size={26}
+        />
+      </div>
+
+      {/* Count UI */}
+      <div className="mt-4">
+        {loading ? (
+          // Skeleton loader UI
+          <div className="h-10 w-24 animate-pulse rounded bg-gray-200"></div>
+        ) : (
+          <p className="text-5xl font-extrabold tracking-wide text-fuchsia-700">
+            {count}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CountCard;
