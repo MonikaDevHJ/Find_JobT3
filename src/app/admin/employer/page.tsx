2e34956416f8f7b8a84d945efbdf7345cdb792d6
Monika H J <monikahj7@gmail.com>
2025-12-17 17:14:49 +0530

    emplyer

--- src/app/admin/employer/page.tsx
+++ src/app/admin/employer/page.tsx
@@ -1,6 +1,43 @@
+"use client";
+
+import { error } from "console";
 import React from "react";
 
+import { useEffect, useState } from "react";
+
+type Employer = {
+  id: string;
+  employerName: string;
+  companyName: string;
+  contactNumber: string;
+  CreatedAt: string;
+};
+
 const adminEmployerpage = () => {
+  const [employer, setEmployer] = useState<Employer[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEmployer = async () => {
+      try {
+        const res = await fetch("/api/admin/employer");
+        if (!res.ok) throw new Error("Failed Fetch Employer");
+        const data = await res.json();
+      } catch (error) {
+        console.log("❌ Error loading Emplyer:", error);
+      } finally {
+      }
+    };
+    fetchEmployer();
+  }, []);
+
+  if (loading) {
+    return (
+      <p className="p-10 text-center text-lg font-semibold">
+        Loading Employer...
+      </p>
+    );
+  }
   return (
     <div className="p-6 md:p-10">
       <h1 className="mb-6 text-2xl font-bold text-fuchsia-700">
@@ -19,19 +56,25 @@ const adminEmployerpage = () => {
           </thead>
 
           <tbody>
-            <tr className="hover:bg-gray-50">
-              <td className="border px-4 py-3">Nanduuuu</td>
-              <td className="border px-4 py-3">anandu@gmail.com</td>
-              <td className="border px-4 py-3">8861484078</td>
-              <td className="border px-4 py-3">15/12/2025</td>
-            </tr>
+            {employer.map((employer) => (
+              <tr key={employer.id} className="hover:bg-gray-50">
+                <td className="border px-4 py-3">{employer.employerName}</td>
+                <td className="border px-4 py-3">{employer.companyName}</td>
+                <td className="border px-4 py-3">{employer.contactNumber}</td>
+                <td className="border px-4 py-3">
+                  {new Date(employer.CreatedAt).toLocaleString()}
+                </td>
+              </tr>
+            ))}
           </tbody>
 
-          <tr>
-            <td colSpan={4} className="p-6 text-center text-gray-500">
-              No Emoployer Found
-            </td>
-          </tr>
+          {employer.length === 0 && (
+            <tr>
+              <td colSpan={4} className="p-6 text-center text-gray-500">
+                No Emoployer Found
+              </td>
+            </tr>
+          )}
         </table>
       </div>
     </div>
