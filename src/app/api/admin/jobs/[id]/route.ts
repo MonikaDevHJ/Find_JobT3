725443a003644e8e178ffc6fbb55153982435dda
Monika H J <monikahj7@gmail.com>
2025-12-22 17:00:48 +0530

    job api is odne

---
+++ src/app/api/admin/jobs/[id]/route.ts
@@ -0,0 +1,24 @@
+import { Param } from "@prisma/client/runtime/library";
+import { NextResponse } from "next/server";
+import { db } from "~/server/db";
+
+export async function  DELETE( req:Request, { params }: { params : {id: string} } ) {
+   
+    try{
+        const jobId = params.id;
+
+        await db.jobPost.delete ({
+            where : {id : jobId}
+        });
+
+        return NextResponse.json({message : "Job Deleted Succcefully"});
+
+    }catch (error) {
+       
+        console.log("❌ Error deleting job:", error)
+        return NextResponse.json(
+            {message : "Failed to delete Job"},
+            {status : 500}
+        );
+    }
+}
