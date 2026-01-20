4e8d89cd91bfe72cc41f791e892583ed9af51bbd
Monika H J <monikahj7@gmail.com>
2026-01-20 18:07:43 +0530

    api is writing

--- src/app/api/uploadingthings/route.ts
+++ src/app/api/uploadingthings/route.ts
@@ -1,7 +1,16 @@
-import {createUploadthing, type FileRouter} from  "uploadthing/next"
-
+import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 // Create an UplodeThing machine that handle uploads
 const f = createUploadthing();
 
+export const ourFileRouter = {
+  comapnyLogo: f({ image: { maxFileSize: "2MB" } }).onUploadComplete(
+    async ({ file }) => {
+      return {
+        url: file.url,
+      };
+    },
+  ),
+} satisfies FileRouter;
 
+export type ourFileRouter = typeof ourFileRouter;
